7e0cfff937198c215481265c0d7a3ceed705a529
bcaffe88 <bruno.caffe@gmail.com>
2025-05-18 15:17:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ async def analisar(texto):
             criterios.append("Posse dominante")
             pontos += 1
 
-        if pontos >= 8:
+        if pontos >= 7:
             veredito = "ENTRAR ✅"
             confianca = "Alta"
             conclusao = "100.00 responsabilidade."
